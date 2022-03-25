8d9f6f3eac76ee20202c24c35287247698739b94
Juan Landim <juan100mil@gmail.com>
2022-03-24 21:06:34 -0300

    automacao da troca de senha

--- cypress/integration/recoverypass.spec.js
+++ cypress/integration/recoverypass.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import fpPage from '../support/pages/forgotpass'
+import rpPage from '../support/pages/resetpass'
 
 describe('Resgate de senha', function () {
     before(function () {
@@ -19,6 +20,21 @@ describe('Resgate de senha', function () {
             fpPage.submit()
             const message = 'Enviamos um e-mail para confirmar a recuperação de senha, cheque sua caixa de entrada.'
             fpPage.toast.shouldHaveText(message)
+        })
+    })
+
+    context('Quando o usuário solicita o resgate', function () {
+        before(function () {
+            cy.postUser(this.data)
+            cy.recoveryPass(this.data.email)
+        })
+        it('Deve pode cadastrar uma nova senha', function () {
+            const token = Cypress.env('recoveryToken')
+            rpPage.go(token)
+            rpPage.form('abc123', 'abc123')
+            rpPage.submit()
+            const message = 'Agora você já pode logar com a sua nova senha secreta.'
+            rpPage.toast.shouldHaveText(message)
         })
     })
 })
