661e2d7d41c00b70e32a33bb3cecd734af947f2d
Juan Landim <juan100mil@gmail.com>
2022-03-31 19:39:50 -0300

    Script de validaçao do agendamento

--- cypress/support/pages/dash/index.js
+++ cypress/support/pages/dash/index.js
@@ -1,8 +1,27 @@
-import {el} from './elements'
+import { el } from './elements'
 import header from '../../components/header'
 class DashPage {
-    constructor(){
-    this.header = header
+    constructor() {
+        this.header = header
+    }
+
+    calendarShouldBeVisible() {
+        cy.get(el.calendar, { timeout: 7000 })
+            .should('be.visible')
+    }
+
+    selectDay(day) {
+        const target = new RegExp('^' + day + '$', 'g')
+        cy.contains(el.boxDay, target)
+            .click({ force: true })
+    }
+
+    appointmentShouldBe(customer, hour){
+        cy.contains('div', customer.name)
+            .should('be.visible')
+            .parent()
+            .contains(el.boxHour, hour)
+            .should('be.visible')
     }
 }
 export default new DashPage()
