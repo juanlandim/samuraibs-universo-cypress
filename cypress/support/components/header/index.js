ce1d80f700771ebbb90576f98e632813a1523827
Juan Landim <juan100mil@gmail.com>
2022-04-11 20:22:46 -0300

    Configuraçao cypress.json

--- cypress/support/components/header/index.js
+++ cypress/support/components/header/index.js
@@ -1,7 +1,7 @@
 import {el} from './elements'
 class Header{
     userLoggedIn(userName) {
-        cy.get(el.fullName, { timeout: 10000 }).should('be.visible').should('have.text', userName)
+        cy.get(el.fullName).should('be.visible').should('have.text', userName)
     }
 }
 export default new Header()
